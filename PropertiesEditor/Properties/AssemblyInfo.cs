94dbe718de955bd28fa4eeaedeeb3ff59015e7c3
SL <semyonl@outlook.com>
2017-07-14 07:53:03 +0700

    Updated project and Roslyn to latest

--- PropertiesEditor/Properties/AssemblyInfo.cs
+++ PropertiesEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PropertiesEditor")]
-[assembly: AssemblyCopyright("Copyright SemyonL ©  2016")]
+[assembly: AssemblyCopyright("Copyright SemyonL © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
